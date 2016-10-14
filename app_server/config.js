e649e4b2e8c6082e592d45b056ea4762608ce693
Sergey Gordeev <sergey-gordeev95@rambler.ru>
2016-10-14 22:06:27 +0400

    alla

--- app_server/config.js
+++ app_server/config.js
@@ -1,27 +1,37 @@
 const express  = require('express')
 //const db = require('./db')
-	require('./models/db');
-	require('./config/passport');
+require('./models/db');
+require('./config/passport');
 
 const passport = require('passport')
 const rotesApi = require('./routes')
 const bodyParser = require('body-parser');
 
-
-
 const app = express()
 
-//app.get('/*', (req,res)=>{
-	//console.log(req.path)
-	//if(req.path == '/') res.redirect(req.path)
-//})
+// app.use('/*', (req,res,next)=>{
+// 	let path = req._parsedUrl.path
+// 	console.log(path)
+// 	if(path == '/') return
+// 	next()
+// })
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static('app_client'))
+app.use('/',express.static('app_client'))
+
 app.use(passport.initialize())
 app.use('/api', rotesApi)
 
 
 module.exports = app
+
+
+let staticRouts = ['home','groups','organisations','members','psychologs','auth']
+initStaticRouts(staticRouts)
+function initStaticRouts(routs){
+	routs.forEach(route =>{
+		app.use(`/${route}*`,express.static('app_client'))
+	})
+}
